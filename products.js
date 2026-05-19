7201eed9461a9a023d6a85df1b391519b74b64bd
AnoNeemus <neemus@AnoNeemuss-Mac-mini.local>
2026-05-19 18:02:04 +0800

    Add: Wall-Mounted Toothbrush and Mouthwash Holder Set: Non-Perforated Dental Storage Rack

--- products.js
+++ products.js
@@ -1,10 +1,10 @@
 const PRODUCTS = [
+  { name: "Wall-Mounted Toothbrush and Mouthwash Holder Set: Non-Perforated Dental Storage Rack", url: "https://s.shopee.sg/9029w3rxIs", emoji: "🛍️" },
   { name: "Biofinest Probiotic 50 Billion CFU Enzyme Supplement 12 + 3 Prebiotics - Diet Slimming Eczema Detox Men Women (60 caps)", url: "https://s.shopee.sg/5q57ZAZUH6", emoji: "🛍️" },
   { name: "Optimum Nutrition, Gold Standard, 100% Whey Protein Powder, 2lbs (907g ) / 5 lbs (2.27 kg) / 10 lbs (4.54 kg)", url: "https://s.shopee.sg/gN0PtDuNn", emoji: "🛍️" },
   { name: "Next Day 122L Foldable Storage Box", url: "https://s.shopee.sg/2LVDnqUm1M", emoji: "🛍️" },
   { name: "TESSAN Universal Travel Adapter with USB+Type C Plug Portable Charger for JP KR ID TH TW AU", url: "https://s.shopee.sg/6pxcAIqFdW", emoji: "🛍️" },
   { name: "Tradishot Immunity Boost Drink | Turmeric Shot (Extra Strength) | Boost Immunity & Well-being [7x35mL]", url: "https://s.shopee.sg/17HQglkZg", emoji: "🛍️" },
   { name: "IndoSedap Oatside Oat Milk - Barista Blend / Chocolate / Coffee | Authentic Original Indonesian Oat Milk (6 x 1L Case)", url: "https://s.shopee.sg/1Vw3cWX7zo", emoji: "🛍️" },
-  { name: "[SG Seller] Acrylic Desk Stand | Out of Office Desk Display |Funny Office Decor |Gift for Colleagues", url: "https://s.shopee.sg/8V5mxaRZCC", emoji: "🛍️" },
-  { name: "Soundcore Sleep A30 by Anker Sleep Earbuds, Smart Active Noise Cancelling Sleep Headphones, Adaptive Snore-Masking System, Ultra-Comfort for Side Sleepers, 45H Playtime", url: "https://s.shopee.sg/AKXQoBEHS0", emoji: "🛍️" }
+  { name: "[SG Seller] Acrylic Desk Stand | Out of Office Desk Display |Funny Office Decor |Gift for Colleagues", url: "https://s.shopee.sg/8V5mxaRZCC", emoji: "🛍️" }
 ];
