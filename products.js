415a4f993c5cd64caa80ad4f0f1072dcbff3f9f9
AnoNeemus <neemus@AnoNeemuss-Mac-mini.local>
2026-05-21 18:01:46 +0800

    Add: [New Launch] SKINTIFIC PDRN Bright & Smooth Moisture Gel - Repair Skin Barrier, Brightening,5000 PPM PDRN, Whitening

--- products.js
+++ products.js
@@ -1,10 +1,10 @@
 const PRODUCTS = [
+  { name: "[New Launch] SKINTIFIC PDRN Bright & Smooth Moisture Gel - Repair Skin Barrier, Brightening,5000 PPM PDRN, Whitening", url: "https://s.shopee.sg/4LGNZGNWWW", emoji: "🛍️" },
   { name: "Mixshop Hand Roll Vacuum Bag for Travel | Space Saving Clothes Storage | SG Ready Stock", url: "https://s.shopee.sg/6feHkTkK7h", emoji: "🛍️" },
   { name: "Night Fat Burner Juice - Wildberry Lavender | Sleep Support & Weight Loss supplement | Fast results", url: "https://s.shopee.sg/40dVZnfQ4x", emoji: "🛍️" },
   { name: "Schneider Avataron C Switch Socket Wine Gold Dark Grey White Landed Condo BTO HDB TML", url: "https://s.shopee.sg/7VDN97qt1U", emoji: "🛍️" },
   { name: "Wall-Mounted Toothbrush and Mouthwash Holder Set: Non-Perforated Dental Storage Rack", url: "https://s.shopee.sg/9029w3rxIs", emoji: "🛍️" },
   { name: "Biofinest Probiotic 50 Billion CFU Enzyme Supplement 12 + 3 Prebiotics - Diet Slimming Eczema Detox Men Women (60 caps)", url: "https://s.shopee.sg/5q57ZAZUH6", emoji: "🛍️" },
   { name: "Optimum Nutrition, Gold Standard, 100% Whey Protein Powder, 2lbs (907g ) / 5 lbs (2.27 kg) / 10 lbs (4.54 kg)", url: "https://s.shopee.sg/gN0PtDuNn", emoji: "🛍️" },
-  { name: "Next Day 122L Foldable Storage Box", url: "https://s.shopee.sg/2LVDnqUm1M", emoji: "🛍️" },
-  { name: "TESSAN Universal Travel Adapter with USB+Type C Plug Portable Charger for JP KR ID TH TW AU", url: "https://s.shopee.sg/6pxcAIqFdW", emoji: "🛍️" }
+  { name: "Next Day 122L Foldable Storage Box", url: "https://s.shopee.sg/2LVDnqUm1M", emoji: "🛍️" }
 ];
