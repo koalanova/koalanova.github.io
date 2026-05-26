23ef65690883b7fb96f1c736849b53b6d3835e79
AnoNeemus <neemus@AnoNeemuss-Mac-mini.local>
2026-05-26 18:03:01 +0800

    Add: Pristine Car Diffuser | Signature & Hotel Scents | Long-lasting Aroma | 8ml Hanging Car Freshener

--- products.js
+++ products.js
@@ -1,10 +1,10 @@
 const PRODUCTS = [
+  { name: "Pristine Car Diffuser | Signature & Hotel Scents | Long-lasting Aroma | 8ml Hanging Car Freshener", url: "https://s.shopee.sg/809nuWIbog", emoji: "🛍️" },
   { name: "Silversea - Premium Atlantic Salmon Fish Fillet Steak 450G/900G Fresh Frozen Seafood - HALAL", url: "https://s.shopee.sg/9zurh3v9Iw", emoji: "🛍️" },
   { name: "(New Arrival) Epinova Silkysoft Printed 980TC Bedsheet without Quilt Cover - Soft & Comfortable - Lightweight", url: "https://s.shopee.sg/5ApawN4aw9", emoji: "🛍️" },
   { name: "Salvo 2/3/4 Layers Laundry Basket With Wheels Bathroom Dirty Clothes Storage Basket Organizers", url: "https://s.shopee.sg/809liUXBLg", emoji: "🛍️" },
   { name: "NANOBAG Reusable, Packable Travel Tote Bag, Sling, Backpack - Ultracompact, Ultralight, Holds 30kg", url: "https://s.shopee.sg/9Kf8J8Bj8U", emoji: "🛍️" },
   { name: "【LATEST】Xiaomi Mi TV Box S (3rd Gen) | Google TV | 4k Ultra HD | Bluetooth 5.2 | Chromecast | 2.4G / 5G | GLOBAL VERSION", url: "https://s.shopee.sg/BQsnCS3P8", emoji: "🛍️" },
   { name: "[BEST SELLING] PRISM+ EvoSonic Ultra | 38,000Pa | AI Dust Detection | 500W BLDC Motor | Cordless Handheld Vacuum Cleaner", url: "https://s.shopee.sg/LkHziLDsK", emoji: "🛍️" },
-  { name: "PRISM+ EvoSonic Pro | 25,000Pa | Green LED | 250W BLDC Motor | Cordless Handheld Vacuum Cleaner with HEPA", url: "https://s.shopee.sg/qgXzXBOcT", emoji: "🛍️" },
-  { name: "Aesthetic Refillable Bottles for Soap and Shampoo MELBOURNE EDITION Aesthetic Refillable Bottles Bathroom Kitchen", url: "https://s.shopee.sg/4VZpMTfHNb", emoji: "🛍️" }
+  { name: "PRISM+ EvoSonic Pro | 25,000Pa | Green LED | 250W BLDC Motor | Cordless Handheld Vacuum Cleaner with HEPA", url: "https://s.shopee.sg/qgXzXBOcT", emoji: "🛍️" }
 ];
