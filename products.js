2b14ca40de59d1c53da90a72b09cce577f24bd6c
AnoNeemus <neemus@AnoNeemuss-Mac-mini.local>
2026-06-01 09:02:20 +0800

    Add: NeeDoh Nice Cube Squishy Fidget Toy

--- products.js
+++ products.js
@@ -1,10 +1,10 @@
 const PRODUCTS = [
+  { name: "NeeDoh Nice Cube Squishy Fidget Toy", url: "https://s.shopee.sg/6VL95yB31y", emoji: "🛍️" },
   { name: "mixshop Baby Wet Wipes, 10 Pkt per Carton Total 800 Sheets, SG ready stock", url: "https://s.shopee.sg/9fI9rz2tSA", emoji: "🛍️" },
   { name: "[NEW LAUNCH] Zenyum Water Flosser Go (Blue/Grey/Pink/Purple)", url: "https://s.shopee.sg/6AiH6SuYOl", emoji: "🛍️" },
   { name: "[NEW] Bose Ultra Open Earbuds", url: "https://s.shopee.sg/809uI1N1MG", emoji: "🛍️" },
   { name: "Fassler Salmon Belly Strip (Skinless) 1kg", url: "https://s.shopee.sg/40dkvauWg0", emoji: "🛍️" },
   { name: "AGIONTEX DryLah! PRO and ULTIMATE KIT  Eco-Friendly Moisture Absorber Dehumidifier PATENT CONTAINERS", url: "https://s.shopee.sg/3LO38Zn9iG", emoji: "🛍️" },
   { name: "Pro 4 Earphone Wireless Bluetooth TWS Headphone Noise Reduction Gaming Headset for Mobile Devices Earbuds", url: "https://s.shopee.sg/7VDbV9Ab9i", emoji: "🛍️" },
-  { name: "ClearUp Microdarts for Acne | Microneedle Patches for Blind Pimples | Pure Hydrocolloid Skincare", url: "https://s.shopee.sg/1qZDkuIKJf", emoji: "🛍️" },
-  { name: "Shieldmonster Stylus Pen for iPad Apple Pencil Palm Rejection iPad Air 11/13/a16/m2/3/Mini 7/Pro 11/13/Gen 10/Air 5/10.2", url: "https://s.shopee.sg/902Nh0AHe2", emoji: "🛍️" }
+  { name: "ClearUp Microdarts for Acne | Microneedle Patches for Blind Pimples | Pure Hydrocolloid Skincare", url: "https://s.shopee.sg/1qZDkuIKJf", emoji: "🛍️" }
 ];
