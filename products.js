f81c4c8861141ba0399c72bf8379154bd4b9220a
AnoNeemus <neemus@AnoNeemuss-Mac-mini.local>
2026-06-06 18:03:08 +0800

    Add: Kiehl's Better Screen UV Serum SPF 50 Facial Sunscreen with Collagen Peptide (50ml)

--- products.js
+++ products.js
@@ -1,10 +1,10 @@
 const PRODUCTS = [
+  { name: "Kiehl's Better Screen UV Serum SPF 50 Facial Sunscreen with Collagen Peptide (50ml)", url: "https://s.shopee.sg/3g16IL4cMk", emoji: "🛍️" },
   { name: "[NEW] Philips 3in1 Airfryer with Steam 5000s NA543/00 | Airfry, Steam, SteamFry | Auto Steam Clean | 7.2L", url: "https://s.shopee.sg/3qKVtWShIQ", emoji: "🛍️" },
   { name: "Moom Health™ Happy Hormones Supplement | Hormone Balance & Fertility Support for PCOS & Endometriosis (30s)", url: "https://s.shopee.sg/6AiPg0HFkr", emoji: "🛍️" },
   { name: "Moom Health™ Daily Wellness Pack | 100% Natural Support for Energy, Stress & Immunity (30-Day Supply)", url: "https://s.shopee.sg/902aS7OwTU", emoji: "🛍️" },
   { name: "Purfresh 200g Mosquito Repellent Scented Gel | Long-Lasting Freshness with IFRA Certified Essential Oils | Car & Room", url: "https://s.shopee.sg/2qRw6z0UdB", emoji: "🛍️" },
   { name: "NEW Mamaforest - Mighty Bubble Clean Powder / Cleaning Agent / Cleaner / Stain Remover /  Eco-friendly / Toppingskids", url: "https://s.shopee.sg/40dsu2hsh5", emoji: "🛍️" },
   { name: "[New Launch]SKINTIFIC Perfect Stay Serum Matte Cushion Foundation SPF50 PA++++ Full Coverage Long-Lasting Makeup", url: "https://s.shopee.sg/8V6HGVqJ0g", emoji: "🛍️" },
-  { name: "#1 HIP Upgrading Door Strip Bottom Sealing Guard Sealer Flexible Insect Stopper Sound Proof Reduce Dust Dirt Noise 1M", url: "https://s.shopee.sg/2g8TihIc6l", emoji: "🛍️" },
-  { name: "Herbal Pharm i.360 Supplements 30 V-Capsules, Essential Vision Support with Lutein, Vitamins", url: "https://s.shopee.sg/AKXtqzDg13", emoji: "🛍️" }
+  { name: "#1 HIP Upgrading Door Strip Bottom Sealing Guard Sealer Flexible Insect Stopper Sound Proof Reduce Dust Dirt Noise 1M", url: "https://s.shopee.sg/2g8TihIc6l", emoji: "🛍️" }
 ];
